d53d5be709fb7dc906ff28f1220a557f7745c02d
tangiambe <tafor@yahoo.com>
2023-08-17 12:35:02 -0400

    added more unit tests

--- test_api.py
+++ test_api.py
@@ -34,35 +34,55 @@ class FlaskAppTestCase(unittest.TestCase):
         })
         self.assertEqual(response.status_code, 302)  # Redirect after successful login
 
-    def test_sb(self):
+    def test_search_books(self):
         response = self.client.get('/search_books')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Search Books', response.data)
 
-    # def test_search_results(self):
-    #     response = self.client.get('/search_results')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Search Results', response.data)
+    def test_search_results(self):
+        response = self.client.get('/search_results')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Search Results', response.data)
 
-    # def test_create_book(self):
-    #     # Log in first
-    #     self.client.post('/login', data={
-    #         'username': 'testuser',
-    #         'password': 'testpassword'
-    #     })
-    #     response = self.client.post('/create_book', data={
-    #         'title': 'Test Book',
-    #         'author': 'Test Author',
-    #         'category': '1',
-    #         'id': '123456',
-    #         'price': '19.99',
-    #         'year': '2023',
-    #         'quantity': '10'
-    #     })
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Book created successfully!', response.data)
+    def test_create_book(self):
+        # Log in first
+        self.client.post('/login', data={
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        response = self.client.post('/create_book', data={
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'category': '1',
+            'id': '123456',
+            'price': '19.99',
+            'year': '2023',
+            'quantity': '10'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Book created successfully!', response.data)
 
-    # Add more test cases...
+    def test_update_book(self):
+        # Log in first
+        self.client.post('/login', data={
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        
+        response = self.client.get('/update_book')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Update Book', response.data)
+
+    def test_delete_book(self):
+        # Log in first
+        self.client.post('/login', data={
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        
+        response = self.client.get('/delete_book')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Delete Book', response.data)
 
 if __name__ == '__main__':
     unittest.main()
